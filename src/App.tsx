ebeb3b89eb11d9960a1389258ad80f4235ba653c
Bouhouch Amjed <7351600-amjed.winshot@users.noreply.gitlab.com>
2022-01-01 23:42:18 +0100

    Add FallBack Suspense

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,44 @@
-import React, { useState } from "react";
+import React, { lazy, Suspense, useState } from "react";
 import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
+import FallBackSuspense from "./components/FallBackSuspense";
 import { AppContext } from "./contexts/AppContext";
 import usePages from "./hooks/usePages";
 import Header from "./Layout/Header";
-import PageContent from "./Layout/PageContent";
 import SideBar from "./Layout/SideBar";
+const PageContent = lazy(() => import("./Layout/PageContent"));
 
 function App() {
   const props = usePages();
   const [isSidebarOpen, setIsSidebarOpen] = useState(true);
   const toggleSideBar = () => setIsSidebarOpen((prev) => !prev);
   return (
-    <AppContext.Provider value={props}>
-      <BrowserRouter>
-        <div className="w-screen flex flex-1 h-screen">
-          <SideBar
-            isSidebarOpen={isSidebarOpen}
-            toggleSideBar={toggleSideBar}
-          />
+    <Suspense fallback={<FallBackSuspense />}>
+      <AppContext.Provider value={props}>
+        <BrowserRouter>
+          <div className="w-screen flex flex-1 h-screen">
+            <SideBar
+              isSidebarOpen={isSidebarOpen}
+              toggleSideBar={toggleSideBar}
+            />
 
-          <Switch>
-            <Route exact path="/">
-              <Redirect to={`/${props.pages[0]._id}`} />
-            </Route>
-            <Route path={`/:id`}>
-              <div className="flex flex-1 flex-col">
-                <Header
-                  isSidebarOpen={isSidebarOpen}
-                  toggleSideBar={toggleSideBar}
-                />
-                <PageContent />
-              </div>
-            </Route>
-          </Switch>
-        </div>
-      </BrowserRouter>
-    </AppContext.Provider>
+            <Switch>
+              <Route exact path="/">
+                <Redirect to={`/${props.pages[0]._id}`} />
+              </Route>
+              <Route path={`/:id`}>
+                <div className="flex flex-1 flex-col">
+                  <Header
+                    isSidebarOpen={isSidebarOpen}
+                    toggleSideBar={toggleSideBar}
+                  />
+                  <PageContent />
+                </div>
+              </Route>
+            </Switch>
+          </div>
+        </BrowserRouter>
+      </AppContext.Provider>
+    </Suspense>
   );
 }
 
---
+++ src/components/FallBackSuspense.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function FallBackSuspense() {
+  return (
+    <div className="flex flex-1 items-center justify-center">
+      <span>Notion clone</span>
+      <span>Loading...</span>
+    </div>
+  );
+}
